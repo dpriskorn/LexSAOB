eb5bc6b80a43753ed32f3218c8f9837d7979930e
Egil D. Priskorn <egils@riseup.net>
2021-04-23 18:10:06 +0200

    Remove old config

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-# Add your credentials from the botpasswords page to your ~/.bashrc or below as
-# strings:
-username = os.environ['LEXUSE_USERNAME']
-password = os.environ['LEXUSE_PASSWORD']
-
-# Global variables
-loglevel = None
